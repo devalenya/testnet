049b712c9c66ba58deb2be2436bdf7aae51cf646
devalenya <31144181+devalenya@users.noreply.github.com>
2022-02-22 12:26:08 +0100

    Update create-nfts.js

--- pages/create-nfts.js
+++ pages/create-nfts.js
@@ -186,49 +186,49 @@ const ipfsClient = create("https://ipfs.infura.io:5001/api/v0");
         <div className="flex justify-center">
             <div className = "w-1/2 flex flex-col pb-11">
                 <input
-                className = "mt-8 borderd rounded p-3 bg-gray-200"
+                className = "mt-8 borderd rounded p-3 bg-pink-200"
                 placeholder="Enter your NFT Name"
                 onChange = {e=>setNftFormInput({...nftFormInput,name:e.target.value})}
                 />
                  <input
-                className = "mt-8 borderd rounded p-3 bg-gray-200"
+                className = "mt-8 borderd rounded p-3 bg-pink-200"
                 placeholder="Enter your NFT Price "
                 value="500" 
                 onChange = {e=>setNftFormInput({...nftFormInput,price:e.target.value})}
                 disabled/>
                  <textarea
-                className = "mt-8 borderd rounded p-3 bg-gray-200"
+                className = "mt-8 borderd rounded p-3 bg-pink-200"
                 placeholder="Enter your NFT Description"
                 onChange ={e=>setNftFormInput({...nftFormInput,description:e.target.value})}
                 />
 
             
                  <div className="grid grid-cols-1 space-y-2 py-4">
-                                    <label className="text-sm font-bold text-gray-500 tracking-wide">Attach the Image</label>
+                                    <label className="text-sm font-bold text-pink-500 tracking-wide">Attach the Image</label>
                         <div className="flex items-center justify-center w-full">
                             <label className="flex flex-col rounded-lg border-4 border-dashed w-full h-60 p-10 group text-center">
                                 <div className="h-full w-full text-center flex flex-col justify-center items-center  ">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 text-blue-400 group-hover:text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 text-blue-400 group-hover:text-pink-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                                     </svg>
                                     <div className="flex flex-auto max-h-48 w-2/5 mx-auto -mt-10">
                                         {
-                                            urlHash?    <img className="has-mask  object-center" src={urlHash} alt="freepik image"/> :                                    <img className="has-mask h-36 object-center" src="https://img.freepik.com/free-vector/image-upload-concept-landing-page_52683-27130.jpg?size=338&ext=jpg" alt="freepik image"/>
+                                            urlHash?    <img className="has-mask  object-center" src={https://exchange.mixontoken.com/wp-content/uploads/2022/02/favicon.png} alt="freepik image"/> :                                    <img className="has-mask h-36 object-center" src="https://img.freepik.com/free-vector/image-upload-concept-landing-page_52683-27130.jpg?size=338&ext=jpg" alt="freepik image"/>
 
 
                                         }
                                     </div>
-                                    <p className="pointer-none text-gray-500 "><span className="text-sm">Drag and drop</span> files here <br /> or <a  id="" className="text-blue-600 hover:underline">select a file</a> from your computer</p>
+                                    <p className="pointer-none text-pink-500 "><span className="text-sm">Drag and drop</span> files here <br /> or <a  id="" className="text-pink-600 hover:underline">select a file</a> from your computer</p>
                                 </div>
                                 <input type="file" onChange={onChange} />
                             </label>
                         </div>
                     </div>
-                            <p className="text-sm text-gray-300">
+                            <p className="text-sm text-pink-300">
                                 <span>File type: png,jpg,jpeg</span>
                             </p>
 
-                            <button className="font-bold bg-yellow-500 mt-5 rounded p-4 text-white"  onClick={createMarketItem}>Submit Your NFT</button>
+                            <button className="font-bold bg-pink-500 mt-5 rounded p-4 text-white"  onClick={createMarketItem}>Mint Your NFT</button>
 
                     
         </div>
