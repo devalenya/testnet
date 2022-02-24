53c8d52ba5fc96319e33408673202adaa4f80706
devalenya <31144181+devalenya@users.noreply.github.com>
2022-02-24 06:08:24 +0100

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -202,13 +202,11 @@ const dashboard  = ()=>{
            </div>
            <div className = "flex justify-center">
                <div className="px-4 " style={{maxWidth:"1600px"}}>
-                <HeadAlert >{{createdNumber:`Number of your NFt Items solded With Us: ${soldItems.length} `,title:"Number Of Sold Items"}}</HeadAlert>
+               
 
               {
                   !soldItems.length ? 
-                   <h1 className=" py-20 text-4xl tracking-tight font-extrabold text-pink-500 sm:text-5xl md:text-6xl">
-                  <span className="block lg:py-3 xl:inline">You Don`t Have Any  Sold Item </span>
-                  </h1> :<>
+                    :<>
 
 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 pt-1">
                    {
