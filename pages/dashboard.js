06908beccec967617499f4ecb9171fb2e9f9065d
devalenya <31144181+devalenya@users.noreply.github.com>
2022-02-24 06:46:02 +0100

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -132,7 +132,7 @@ const dashboard  = ()=>{
 
  
            }else{
-               window.alert("You are at Wrong Netweok, Connect with BINANCE SMART CHAIN TESTNET")
+               window.alert("You are at Wrong Netweok, Connect with Roposten Please")
            }
 
 
@@ -160,8 +160,8 @@ const dashboard  = ()=>{
 
               {
                   !creathedItems.length ? 
-                   <h1 className=" py-20 text-4xl tracking-tight font-extrabold text-pink-500 sm:text-5xl md:text-6xl">
-                  <span className="block lg:py-3 xl:inline">You Don`t Have Any  Nft Item Yet</span>
+                   <h1 className=" py-20 text-4xl tracking-tight font-extrabold text-yellow-500 sm:text-5xl md:text-6xl">
+                  <span className="block lg:py-3 xl:inline">You Don`t Have Any  Nft Item </span>
                   </h1> :<>
 
 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 pt-1">
@@ -175,13 +175,15 @@ const dashboard  = ()=>{
       <div className="w-80 mt-24 m-auto lg:mt-16 max-w-sm">
         <img src={item.image} alt=""className="rounded-t-2xl shadow-2xl lg:w-full 2xl:w-full 2xl:h-44 object-cover"/>
         <div className="bg-white shadow-2xl rounded-b-3xl">
-          <h2 className="text-center text-pink-800 text-2xl font-bold pt-6">{item.name}</h2>
+          <h2 className="text-center text-gray-800 text-2xl font-bold pt-6">{item.name}</h2>
           <div className="w-5/6 m-auto">
-            <p className="text-center text-pink-500 pt-5"> {item.description}</p>
+            <p className="text-center text-gray-500 pt-5"> {item.description}</p>
           </div>
           <div className="grid grid-cols-4 w-72 lg:w-5/6 m-auto bg-indigo-50 mt-5 p-4 lg:p-4 rounded-2xl">
-           
-           </div>
+            
+            
+        
+          </div>
     
           <div className="text-center m-auto mt-6 w-full h-1">
           </div>
@@ -204,13 +206,13 @@ const dashboard  = ()=>{
            </div>
            <div className = "flex justify-center">
                <div className="px-4 " style={{maxWidth:"1600px"}}>
-               <HeadAlert >{{createdNumber:`Number of your NFt Items solded: ${soldItems.length} `,title:"Number Of Sold Items"}}</HeadAlert>
+                <HeadAlert >{{createdNumber:`Number of your NFt Items solded: ${soldItems.length} `,title:"Number Of Sold Items"}}</HeadAlert>
 
               {
                   !soldItems.length ? 
-                   <h1 className=" py-20 text-4xl tracking-tight font-extrabold text-yellow-500 sm:text-5xl md:text-6xl">
-                  <span className="block lg:py-3 xl:inline">You Don`t Have Any  Sold Item </span>
-                  </h1> :<>:<>
+                   <h1 className=" py-20 text-4xl tracking-tight font-extrabold text-pink-500 sm:text-5xl md:text-6xl">
+                  <span className="block lg:py-3 xl:inline"> NFT Selling is Unavailable</span>
+                  </h1> :<>
 
 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 pt-1">
                    {
@@ -227,8 +229,17 @@ const dashboard  = ()=>{
           <div className="w-5/6 m-auto">
             <p className="text-center text-pink-500 pt-5"> {item.description}</p>
           </div>
-          <div className="grid grid-cols-4 w-72 lg:w-5/6 m-auto bg-indigo-50 mt-5 p-4 lg:p-4 rounded-2xl"> </div>
-       
+          <div className="grid grid-cols-4 w-72 lg:w-5/6 m-auto bg-indigo-50 mt-5 p-4 lg:p-4 rounded-2xl">
+            <div className="col-span-1 flex">
+ 
+              <img className="flex justify-center w-15 lg:w-12" src="https://img.icons8.com/fluency/48/000000/ethereum.png" alt="music icon"/>
+            </div>
+            <div className="col-span-2 pt-1 ">
+              <p className="text-pink-800 font-bold lg:text-sm">Price</p>
+              <p className="text-pink-500 text-sm font-bold">{item.price} ETH</p>
+            </div>
+        
+          </div>
     
           <div className="text-center m-auto mt-6 w-full h-1">
           </div>
