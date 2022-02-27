cd79cf7b8ff133c87be95d7213e7fff80c9b9bf6
devalenya <31144181+devalenya@users.noreply.github.com>
2022-02-27 21:24:04 +0100

    Update HeadSection.js

--- components/HeadSection.js
+++ components/HeadSection.js
@@ -129,6 +129,16 @@ const HeadSection = () => {
                </div>
 
 
+
+                 <center><div className="sm:text-center lg:text-left">
+                   <h1 className="text-4xl tracking-tight font-extrabold text-pink-500 sm:text-5xl md:text-6xl">
+                       <span className="block lg:py-3 xl:inline">NFT Minting Made Easy</span>
+                       <span className="block dark:text-white xl:inline">Exknowplay NFT</span>
+                       </h1>
+                       <p className="mt-3 text-base text-white sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0" >
+                       on the world's first & largest NFT marketplace, Exknowplay NFT is a native token, acquired by simply holding NFT, that can be sold.   </p>
+                   </div></center>
+
                            </section>
                            
                        </div></div>
