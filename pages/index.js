7b804042b16c04a6f99a681a68dad018895ccdf9
devalenya <31144181+devalenya@users.noreply.github.com>
2022-02-28 03:58:21 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -228,6 +228,40 @@ const buyNFT = async (nftItem)=>{
            }
          
 
+           
+          <div className="relative max-w-7xl mx-auto px-4 dark:bg-bgcolor">
+          <section>
+              <div className="relative dark:bg-bgcolor overflow-hidden">
+               <div className="max-w-7xl mx-auto">
+               <div className="relative z-10 pb-8 indigo-900 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+                   <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+                   </div>
+                   <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                   <div className="sm:text-center lg:text-left">
+                   <h1 className="text-4xl tracking-tight font-extrabold text-pink-500 sm:text-5xl md:text-6xl">
+                       <span className="block lg:py-3 xl:inline" >Why Mint With</span>
+                       <span className="block dark:text-white xl:inline"> Exknowplay NFT</span>
+                       </h1>
+                       <p className="mt-3 text-base text-white sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0" >
+                       on the world's first & largest NFT marketplace, Exknowplay NFT is a native token, acquired by simply holding NFT, that can be sold  on the world's first & largest NFT marketplace, Exknowplay NFT is a native token, acquired by simply holding NFT, that can be sold. </p>
+                    
+                           
+                   </div>
+                   </main>
+               </div>
+               </div>
+               <div className="lg:absolute lg:inset-y-3 lg:right-0 lg:w-1/2">
+               <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://ardapps.com/wp-content/uploads/2021/11/head-1.png" alt="headerSection Image" />
+               </div>
+               </div>
+                   </section>
+                           
+                       </div>
+           
+           
+           
+           
+           
 
            <div className = "flex justify-center">
              
